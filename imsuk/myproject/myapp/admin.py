42610413751e8fdc339f3c53fdc1e50e72ee389e
Kanasawan <kanasawan18@gmail.com>
2025-05-10 00:03:23 +0700

    update admin.py

--- imsuk/myproject/myapp/admin.py
+++ imsuk/myproject/myapp/admin.py
@@ -1,32 +1,46 @@
 from django.contrib import admin
-from .models import Store, MenuItem, Address, PaymentMethod, Order, OrderItem, FavoriteStore
+from .models import (
+    Store,
+    MenuItem,
+    Address,
+    PaymentMethod,
+    Order,
+    OrderItem,
+    FavoriteStore
+)
+
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category']
-    search_fields = ['name', 'category']
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category')
+
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'store', 'original_price', 'sale_price']
-    list_filter = ['store']
-    search_fields = ['name']
+    list_display = ('name', 'store', 'original_price', 'sale_price', 'available_time_start', 'available_time_end')
+    list_filter = ('store',)
+    search_fields = ('name', 'description')
+
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name', 'province', 'is_default']
-    list_filter = ['province']
-    search_fields = ['name', 'user__username']
+    list_display = ('user', 'name', 'province', 'is_default')
+    search_fields = ('user__username', 'name', 'province')
+
 @admin.register(PaymentMethod)
 class PaymentMethodAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name']
-    search_fields = ['name', 'user__username']
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
+    list_display = ('user', 'name', 'detail')
+    search_fields = ('user__username', 'name')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'created_at', 'is_paid', 'total_price']
-    list_filter = ['is_paid']
-    inlines = [OrderItemInline]
+    list_display = ('user', 'created_at', 'is_paid', 'total_price')
+    list_filter = ('is_paid',)
+    search_fields = ('user__username',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'menu_item', 'quantity')
+
 @admin.register(FavoriteStore)
 class FavoriteStoreAdmin(admin.ModelAdmin):
-    list_display = ['user', 'store']
-    search_fields = ['user__username', 'store__name']
+    list_display = ('user', 'store')
+    search_fields = ('user__username', 'store__name')
